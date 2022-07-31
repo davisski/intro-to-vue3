576c53c79bac266bd1144cd1b9f34cfe1ec1f253
Dāvis Šķipsna <skipsna.davis@gmail.com>
2022-07-31 20:29:13 +0200

    #9 - Binding image property to img tag src attribute.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      description: "This is the description of socks.",
+      image: "./assets/images/socks_green.jpg",
     };
   },
 });
