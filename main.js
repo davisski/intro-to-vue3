8e28da283cc4023af8a468cb182ef560c817dc19
Dāvis Šķipsna <skipsna.davis@gmail.com>
2022-08-01 20:45:01 +0200

    #19 - Created product display component.

--- main.js
+++ main.js
@@ -2,43 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       cart: 0,
-      product: "Socks",
-      brand: "Vue Mastery",
-      selectedVariant: 0,
-      details: ["50% cotton", "30% wool", "20% polyester"],
-      variants: [
-        {
-          id: 1,
-          color: "green",
-          image: "./assets/images/socks_green.jpg",
-          quantity: 50,
-        },
-        {
-          id: 2,
-          color: "blue",
-          image: "./assets/images/socks_blue.jpg",
-          quantity: 0,
-        },
-      ],
     };
   },
-  methods: {
-    addToCart() {
-      this.inStock ? (this.cart += 1) : null;
-    },
-    updateSelected(sock) {
-      this.selectedVariant = sock;
-    },
-  },
-  computed: {
-    title() {
-      return this.brand + " " + this.product;
-    },
-    image() {
-      return this.variants[this.selectedVariant].image;
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].quantity > 0;
-    },
-  },
+  methods: {},
 });
